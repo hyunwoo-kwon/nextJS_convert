98eb6591014da9e384ac3dfba0eeee4f7ea6f0ee
khw <avjhome@naver.com>
2022-09-12 10:04:07 +0900

    20220912_1003_KHW 1.finish 1.5

---
+++ components/1_5_NavBar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+import style from "./NavBar.module.css";
+
+export default function NavBar(){
+    const router = useRouter();
+    console.log(router)
+
+    return (
+        <nav>
+            <Link href="/">
+                <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+            </Link>
+            <Link href="/about">
+                <a className={router.pathname === "/about" ? "active" : ""}>about</a>
+            </Link>
+            {
+                //스타일은 html tag
+            }
+            {/*style jsx plugin installed*/}
+            {/* eslint-disable-next-line react/no-unknown-property */}
+            <style jsx>{`
+                nav {
+                    background-color: tomato;
+                }
+                a {
+                    text-decoration: none;
+                }
+                .active {
+                    color: yellow;
+                }
+            `}</style>
+        </nav>
+    )
+}
